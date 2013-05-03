c4df0f8cc330518b06674b61f77686dc8961c5c7
greg Sochanik <greg@sochanik.com>
2013-05-03 14:54:13 +0100

    Added some jquery api demos that use the buynow widget

---
+++ examples/scripts/demo/morelikethis.js
@@ -0,0 +1,60 @@
+﻿var url = $.url(location.href),
+    releaseId = url.param("releaseId"),
+    releaseTitle = url.param("releaseTitle");
+
+$("head > title").html(releaseTitle);
+$("#title").html("More like "+releaseTitle);
+
+function MoreLikeThisDemo() {
+    this.settings = {
+        apiUrl: "http://api.7digital.com/1.2/release/recommend?oauth_consumer_key=YOUR_KEY_HERE&type=album&pagesize=50&imagesize=175&releaseId=",
+        displayElement: $("#sidebar"),
+        templateElement: $("#tpl-release-search-item"),
+    };
+
+    this.PageNum = 1;
+}
+
+MoreLikeThisDemo.prototype.RunSearch = function(query, pagenum) {
+    var settings = this.settings;
+    $.support.cors = true;
+    $.get(settings.apiUrl + query + "&page=" + pagenum, function (data) {
+        var template = _.template(settings.templateElement.html());
+        _.each($(data).find("recommendedItem > release"), function (release) {
+            var $release = $(release);
+            var recommendation = {
+                id: $release.attr("id"),
+                image: $release.find("image").text(),
+                artist: {
+                    appearsAs: $release.find("artist").find("appearsAs").text(),
+                    id: $release.find("artist").attr("id"),
+                },
+                title: $release.find("title").text(),
+                type: $release.find("type").text(),
+                price: {
+                    formattedPrice: $release.find("price").find("formattedPrice").text()
+                }
+            };
+            settings.displayElement.append(template(recommendation));
+        }, this);
+        document.blocky.setup();
+    })
+    .error(function (err, textStatus, errorThrown) {
+        settings.displayElement.html("<p>An error occured : " + errorThrown + "</p>");
+        throw "Could not load search from the api:";
+    });
+    return this;
+};
+
+MoreLikeThisDemo.prototype.LoadNext = function() {
+    this.PageNum += 1;
+    this.RunSearch(releaseId, this.PageNum);
+};
+
+var moreLikeThisDemo = new MoreLikeThisDemo().RunSearch(releaseId, 1);
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        moreLikeThisDemo.LoadNext();
+    }
+});
---
+++ examples/scripts/demo/search.js
@@ -0,0 +1,71 @@
+﻿function TrackSearchDemo() {
+    this.settings = {
+        apiUrl: "http://api.7digital.com/1.2/track/search?oauth_consumer_key=YOUR_KEY_HERE&pagesize=50&imagesize=175&q=",
+        displayElement: $("#sidebar"),
+        templateElement: $("#tpl-track-list-item"),
+        searchTextFieldElement: $("#searchquery"),
+        defaultQuery: "great gig in the sky"
+    };
+    this.PageNum = 1;
+}
+
+TrackSearchDemo.prototype.Setup = function() {
+    var settings = this.settings,
+        that = this;
+    
+    settings.searchTextFieldElement.val(settings.defaultQuery);
+    settings.searchTextFieldElement.keyup(function (event) {
+        var querytext = event.currentTarget.value;
+        settings.displayElement.html("");
+        that.PageNum = 1;
+        if (querytext !== "") {
+            $(event.currentTarget).attr('class', 'pleasewait');
+            that.RunSearch(querytext, that.PageNum);
+        }
+    });
+    return this;
+};
+
+TrackSearchDemo.prototype.RunSearch = function(query, pagenum) {
+    var settings = this.settings,
+        template = _.template(settings.templateElement.html());
+    
+    if (query == "" || query == undefined) {
+        query = settings.defaultQuery;
+    }
+
+    if (pagenum === undefined) {
+        pagenum = 1;
+    }
+
+    $.support.cors = true;
+    $.get(settings.apiUrl + query + "&page=" + pagenum, function (data) {
+        if (data.status == "ok") {
+            _.each(data.searchResults.searchResult, function (searchResult) {
+                settings.displayElement.append(template(searchResult.track));
+            }, this);
+            settings.searchTextFieldElement.attr('class', '');
+            document.blocky.setup();
+        }
+    }, "json")
+    .error(function (err, textStatus, errorThrown) {
+        settings.displayElement.html("<p>An error occured : " + errorThrown + "</p>");
+        throw "Could not load search from the api:";
+    });
+    return this;
+};
+
+TrackSearchDemo.prototype.LoadNext = function() {
+    var settings = this.settings;
+    var querytext = settings.searchTextFieldElement.val();
+    this.PageNum += 1;
+    this.RunSearch(querytext, this.PageNum);
+};
+
+var trackSearchDemo = new TrackSearchDemo().Setup().RunSearch("", 1);
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        trackSearchDemo.LoadNext();
+    }
+});
